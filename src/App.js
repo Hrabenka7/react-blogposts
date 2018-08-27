f0b4bf5889477b57f5bb572cc9ec7f4ba3436e37
Marketa Skoloudikova <marketa.skoloudikova@forcemanager.net>
2018-08-27 13:00:39 +0200

    setting up Routing Package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom'; // for using routing
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Blog />
       </div>
+      </BrowserRouter>
     );
   }
 }
