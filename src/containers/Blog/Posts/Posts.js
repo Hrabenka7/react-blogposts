bd23150a7aa44b80b250f4caf9d49d135460ddf9
Marketa Skoloudikova <marketa.skoloudikova@forcemanager.net>
2018-08-28 12:15:02 +0200

    add routing properties via withRouter

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -10,6 +10,7 @@ class Posts extends Component {
     }
 
     componentDidMount () {
+        console.log(this.props)
         axios.get('/posts') // error handling if URL is wrong
         .then(response => {
             const posts = response.data.slice(0,4); // first 4 posts
