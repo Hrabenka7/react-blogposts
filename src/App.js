b10b171a5b94c3a583e2f3f50874c738c5162d36
Marketa Skoloudikova <marketa.skoloudikova@forcemanager.net>
2018-09-18 16:24:49 +0200

    serving app from subdirectory

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="/my-app"> whenever serving app form subdirectory set basename!
       <BrowserRouter>
       <div className="App">
         <Blog />
